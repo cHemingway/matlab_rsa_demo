858a23ee141efbfe7a52c3e953573224ec9dc8c0
Chris Hemingway <chris.hemingway@gmail.com>
2019-05-06 21:07:37 +0100

    Add option to disable plaintext length checking

--- rsa_encrypt.m
+++ rsa_encrypt.m
@@ -1,12 +1,14 @@
-% Author: Chris Hemingway
-% Part of matlab_rsa project
-
-function c = rsa_encrypt(plaintext, Kp)
+function c = rsa_encrypt(plaintext, Kp, varargin)
 %RSA_ENCRYPT encrypts char/string plaintext to ciphertext c using public key Kp
 %   c = rsa_encrypt(plaintext, Kp)
+%
+%   Length checking can also be disabled (enabled by default)
+%   rsa_encrypt(very_long_message,Kp,'checkLength',false)
+%
 %   See also rsa_keygen, rsa_decrypt
 
 % TODO use binary modular exponentiation instead of needing VPI library
+
 
 % Convert text to a number
 if isa(plaintext, 'char') || isa(plaintext, 'string')
@@ -15,9 +17,19 @@ else
     p = plaintext;
 end
 
+% Check inputs
+defaultcheckLength = false;
+parser = inputParser;
+addParameter(parser,'checkLength',defaultcheckLength, @islogical);
+
+parse(parser,varargin{:});
+
+
 % Check input is not bigger than key
-if length(plaintext) * 8 > ceil(log2(Kp.n))
-    error("Plaintext is longer than key");
+if parser.Results.checkLength
+    if length(plaintext) * 8 > ceil(log2(Kp.n))
+        error("Plaintext is longer than key");
+    end
 end
 
 % We have not added padding, which effectively means zero padding is used
@@ -27,5 +39,8 @@ end
 c = powermod(p,Kp.e,Kp.n);
 
 end
+
+% Author: Chris Hemingway
+% Part of matlab_rsa project
 
 
