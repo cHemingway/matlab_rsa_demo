29c917e1fb27db3af2942b958cc11521c077ff61
Chris Hemingway <chris.hemingway@gmail.com>
2019-05-06 22:26:20 +0100

    Add ability to disable converting back to text in rsa_decrypt

--- rsa_decrypt.m
+++ rsa_decrypt.m
@@ -1,16 +1,30 @@
 % Author: Chris Hemingway
 % Part of matlab_rsa project
 
-function plaintext = rsa_decrypt(c, Ks)
+function plaintext = rsa_decrypt(c, Ks, varargin)
 %RSA_DECRYPT decrypt VariablePrecisionInt c using private key Ks to chars
-% plaintext = rsa_decrypt(c, Ks)
+%   plaintext = rsa_decrypt(c, Ks)
+%
+%   By default, it will presume you are decrypting text. To disable
+%   number = rsa_decrypt(c, Ks, 'isText', false);
 %   See also rsa_keygen, rsa_decrypt
+
+
+% Check inputs
+defaultisText = true;
+parser = inputParser;
+addParameter(parser,'isText',defaultisText, @islogical);
+parse(parser,varargin{:});
 
 % Decrypt, p = c^d mod n
 % Using Matlab powermod function as faster
 p = powermod(c,Ks.d,Ks.n);
 
-% Convert from number back to chars
-plaintext = vpi2char(p);
+if parser.Results.isText
+    % Convert from number back to chars
+    plaintext = vpi2char(p);
+else
+    % Don't convert it
+    plaintext = p;
 
 end
